4c6d2604250378a42281847d15100c2dca29cbf1
Ulonna Godspower <108946078+GodspowerGP@users.noreply.github.com>
2024-10-12 15:41:48 +0100

    changes

---
+++ sendEmail.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $firstname = $_POST['firstname'];
+  $lastname = $_POST['lastname'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $state = $_POST['state'];
+  $message = $_POST['message'];
+
+  $to = "degppowerhouse@gmail.com";
+  $subject = "New Contact Form Submission";
+  $body = "Name: $firstname $lastname\nEmail: $email\nPhone: $phone\nState: $state\nMessage:\n$message";
+  $headers = "From: $email";
+
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Message sent successfully!";
+  } else {
+    echo "Message failed to send.";
+  }
+}
+?>
